a4b150105837f96247caa356760b2e6d1b04cc66
Alexey <beastrock@mail.ru>
2016-10-23 23:38:01 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -2,8 +2,8 @@ import requests
 import re
 
 
-def upload_pass_base(fp):
-    r = requests.get(fp)
+def upload_pass_base(file_path):
+    r = requests.get(file_path)
     return r.text
 
 
@@ -18,7 +18,6 @@ def get_password_strength(password):
     RECOMMENDED_PASSWORD_LENGTH = 14
     FIVE_SYMBOLS = 5
     pass_properties = []
-    # recommended length
     if len(password) >= RECOMMENDED_PASSWORD_LENGTH:
         length = 2
     elif (RECOMMENDED_PASSWORD_LENGTH * 0.5) <= len(password) < RECOMMENDED_PASSWORD_LENGTH:
@@ -28,30 +27,26 @@ def get_password_strength(password):
     else:
         length = -1
     pass_properties.append(length)
-    # inclusion from the base of bad passwords
-    base = upload_pass_base('https://beastrock.github.io/blackpass.txt')
+    base = upload_pass_base('https://beastrock.github.io/pass_base.txt')
     if not (password in base):
         being_in_base = 2
     else:
         being_in_base = -1
     pass_properties.append(being_in_base)
-    # inclusion of one or more numerical digits
-    if (1 < len(re.findall("[\d]", password))) < (len(password)):
+    if 1 < len(re.findall("[\d]", password)) < (len(password)):
         digits = 2
-    elif (len(re.findall("[\d]", password))) == 1:
+    elif len(re.findall("[\d]", password)) == 1:
         digits = 1
-    elif (len(re.findall("[\d]", password))) == 0:
+    elif len(re.findall("[\d]", password)) == 0:
         digits = 0
-    elif len(re.findall("[\d]", password)) == (len(password)):
+    elif len(re.findall("[\d]", password)) == len(password):
         digits = -1
     pass_properties.append(digits)
-    # inclusion of special characters
     if re.search("[\W\s]", password):
         special_characters = 2
     else:
         special_characters = 0
     pass_properties.append(special_characters)
-    # the use of both upper-case and lower-case letters (case sensitivity)
     if not (password.islower() or password.isupper()):
         case_sensitive = 2
     else:
@@ -66,26 +61,21 @@ def print_result(pass_properties):
     digits = pass_properties[2]
     special_characters = pass_properties[3]
     case_sensitive = pass_properties[4]
-
-    if -1 in pass_properties:
-
-    if length == 2:
+    if length == 2 and being_in_base != -1:
         print('long password length: +2 points')
-    elif length == 1:
+    elif length == 1 and being_in_base != -1:
         print('medium password length: +1 point')
-    elif length == 0:
+    elif length == 0 and being_in_base != -1:
         print('short password length: 0 points')
-    elif length == -1:
+    elif length == -1 and being_in_base != -1:
         print('Very short password: 1/10 ')
         return None
-    #
     if being_in_base == 2:
         print("password isn't in bad passwords base: +2 points")
     elif being_in_base == -1:
         print("Password is in the base of bad passwords.\n"
               "it is weak like a boxer after 10 rounds: 1/10\n")
         return None
-    #
     if digits == 2:
         print("includes one or more digits: +2 points")
     elif digits == 0:
@@ -93,19 +83,16 @@ def print_result(pass_properties):
     elif digits == 1:
         print("includes one digit: 1 point")
     elif digits == -1:
-        print("Password have to include letters: 1/10")
-    #
+        print("No letters: 0 points ")
     if special_characters == 2:
         print("included special characters: +2 points")
     elif special_characters == 0:
         print("no special characters: 0 points")
-
     if case_sensitive == 2:
         print('two cases are used: +2 points')
     elif case_sensitive == 0:
         print('only one case is used: 0 points')
-
-    password_strength = 1
+    password_strength = sum(pass_properties)
     message = 'Your password strength, is {}/10!'.format(password_strength)
     print(message)
     return None
